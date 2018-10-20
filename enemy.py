3d5ca021259b01a9b8f579518180ed0266e9ac40
Jack Glaister <jackglaister@gmail.com>
2018-10-20 20:53:05 +0100

    enemy and dragonenemy combined

--- enemy.py
+++ enemy.py
@@ -1,4 +1,11 @@
-def __init__(self, name, xp, health):
-	self.name=name
-	self.xp = xp
-	self.health = health
+class enemy()
+	def __init__(self, name, xp, health):
+		self.name=name
+		self.xp = xp
+		self.health = health
+
+class dragonenemy(enemy):
+	def __init__(self):
+		self.name = "dragon"
+		self.health = 200
+		self.xp = 150
