8d694e7e3cf7f86a3278ba2c495bc4c02fedc28c
jackglaister <44065198+jackglaister@users.noreply.github.com>
2018-10-20 21:16:04 +0100

    Update room.py

--- room.py
+++ room.py
@@ -1,4 +1,18 @@
-def __init__(self,name,x,y,fl,fr,bl,br):
-	self.name=name
-	self.number=((x,y))
-	self.exits={"forward left":fl, "forward right":fr, "backward left":bl, "backward right":br}
+class room():
+	def __init__(self,name,x,y,fl,fr,bl,br):
+		self.name=name
+		self.number=((x,y))
+		self.exits={"forward left":fl, "forward right":fr, "backward left":bl, "backward right":br}
+
+	def __init__(self,name,description,x,y):
+		self.description = description
+		self.name=name
+		self.x=x
+		self.y=y
+	def exitsGen(self):
+		self.exits["forward left"] = room("random name", "random description", x+1, y)
+		self.exits["forward right"] = room("random name", "random description", x+2, y)
+		self.exits["backward left"] = room("random name", "random description", x-1, y)
+		self.exits["backward right"] = room("random name", "random description", x-2, y)
+	def generate(self, x)
+		exitsGen()
