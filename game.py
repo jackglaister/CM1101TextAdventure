f016adde61b8d51404cc7fb9d6bf522491e23611
Jack Glaister <jackglaister@gmail.com>
2018-10-21 23:55:45 +0100

    new commit

--- game.py
+++ game.py
@@ -1,10 +1,17 @@
 import player, room
 def GameControl():
 	player = CreatePlayer()
+	current_room = starterRoom()
 
 def CreatePlayer():
 	name = input("Please enter your name: ")
 	gender = input("Please enter your gender: ")
 	return player.player(name,gender)
+
+def starterRoom():
+	return room.room("start","description",0,0)
+
+def menu(current_room):
+	print(current_room.name+"\n"+current_room.description)
 
 GameControl()
--- room.py
+++ room.py
@@ -1,18 +1,14 @@
 class room():
-	def __init__(self,name,x,y,fl,fr,bl,br):
-		self.name=name
-		self.number=((x,y))
-		self.exits={"forward left":fl, "forward right":fr, "backward left":bl, "backward right":br}
-
-	def __init__(self,name,description,x,y):
-		self.description = description
-		self.name=name
-		self.x=x
-		self.y=y
-	def exitsGen(self):
+	def exitsGen(self): #add exits of rooms from a text file for the fixed room positions
 		self.exits["forward left"] = room("random name", "random description", x+1, y)
 		self.exits["forward right"] = room("random name", "random description", x+2, y)
 		self.exits["backward left"] = room("random name", "random description", x-1, y)
 		self.exits["backward right"] = room("random name", "random description", x-2, y)
-	def generate(self, x)
+	
+	def generate(self, x): 
 		exitsGen()
+
+	def __init__(self, name, description, x, y):	#init class (x and y are location variables)
+		self.name=name
+		self.description=description
+		self.number=((x,y))
