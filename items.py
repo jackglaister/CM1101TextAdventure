795ccfd3099e6bc9aefc6298d030240e639b652c
Joe Creek <43994225+CreekKraken@users.noreply.github.com>
2018-10-22 13:46:59 +0100

    add types and effectiveness values

--- items.py
+++ items.py
@@ -1,13 +1,12 @@
 class item():
-    def __init__(self,item_id,name,weight,description,value,type):
-        self.id=item_id
-        self.name=name
-        self.weight=weight
-        self.description=description
-        self.value=value
-        self.type=type
-        
-#item template is item(id,name,weight,description,value)
-#eg reference "items.caviar" to call upon it in another file (after importing items)
+    def __init__(self,item_id,name,weight,description,value,type,potency):
+        self.id=item_id         #unique item ID
+        self.name=name          #item name
+        self.weight=weight      #appropriate weight
+        self.description=description    #description of item
+        self.value=value        #value of item in gold
+        self.type=type          #type e.g. food, weapon, misc
+        self.potency=potency    #effectiveness (how much food heals, how much damage a weapon does)
 
-caviar=item("caviar","caviar",0.1,"salt-cured fish ovum, a rare delicacy",100,"food")
+#eg reference "items.caviar" to call upon it in another file (after importing items)
+caviar=item("caviar","caviar",0.1,"salt-cured fish ovum, a rare delicacy",100,"food",30)
