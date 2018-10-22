d53e96c996443e8080b47893882067a3f10753a9
jackglaister <jackglaister@gmail.com>
2018-10-22 13:03:59 +0100

    added kirill

--- enemy.py
+++ enemy.py
@@ -4,8 +4,15 @@ class enemy():
 		self.xp = xp
 		self.health = health
 
-class dragonenemy(enemy):
+class dragonling(enemy):
 	def __init__(self):
 		self.name = "dragon"
 		self.health = 200
 		self.xp = 150
+
+class kirill(dragonling):
+	def __init__(self):
+		self.name = "kirill"
+		self.health = 1000
+		self.favourite = "caviar"
+		self.weakness = "oysters"
