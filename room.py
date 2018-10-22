619f0482ae169ef1339cb329f4e3f7f00b6b58fb
Jack Glaister <jackglaister@gmail.com>
2018-10-22 20:34:02 +0100

    updated slightly

--- room.py
+++ room.py
@@ -1,12 +1,6 @@
 class room():
-    def exitsGen(self): #add exits of rooms from a text file for the fixed room positions
-        self.exits["forward left"] = room("random name", "random description", x+1, y)
-        self.exits["forward right"] = room("random name", "random description", x+2, y)
-        self.exits["backward left"] = room("random name", "random description", x-1, y)
-        self.exits["backward right"] = room("random name", "random description", x-2, y)
-    def generate(self, x): 
-        exitsGen()
-    def __init__(self, name, description, x, y):	#init class (x and y are location variables)
+    def __init__(self, name, description, x, exits):	#init class (x and y are location variables)
         self.name=name
         self.description=description
-        self.number=((x,y))
+        self.number=(x)
+	self.exits=exits
---
+++ roomstore.py
@@ -0,0 +1,12 @@
+from room import room
+rooms = [
+    room("the royal kitchen","you are a young servant known by the name "+ current_player.name +".\nYou have fallen ill to a grave illness and have decided to leave\nyour terrible life and seek fame and fortune.",0,0),
+    "random",
+    "random",
+    "random",
+]
+
+RandRooms = [
+    room("Dark Road","An old homeless lady is sat on the edge of a path with a sword she told you was very valuable and was passed down from a long line of ancestors and is offering it for sale for 100 gold",0,0),
+    room("The Highway","An old man is trying to cross a 4 lane wide highway ahead of you",0,0),
+]
