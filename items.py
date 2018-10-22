fa3fb0f7333c5e69b052aa7504647f2353d5b969
Joe Creek <43994225+CreekKraken@users.noreply.github.com>
2018-10-22 13:21:19 +0100

    Add files via upload
    item definition and classing using basic OOP

---
+++ items.py
@@ -0,0 +1,9 @@
+class item():
+    def __init__(self,item_id,name,weight,description,value):
+        self.id=item_id
+        self.name=name
+        self.weight=weight
+        self.description=description
+        self.value=value
+
+item_caviar=item("caviar","caviar",0.1,"salt-cured fish ovum, a rare delicacy",100)
