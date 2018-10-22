ac1f1ee6160c80b71d9e9f1e836e69fc47171dc9
Joe Creek <43994225+CreekKraken@users.noreply.github.com>
2018-10-22 13:23:43 +0100

    Update items.py

--- items.py
+++ items.py
@@ -1,9 +1,11 @@
 class item():
-    def __init__(self,item_id,name,weight,description,value):
+    def __init__(self,item_id,name,weight,description,value,type):
         self.id=item_id
         self.name=name
         self.weight=weight
         self.description=description
         self.value=value
+        self.type=type
 
-item_caviar=item("caviar","caviar",0.1,"salt-cured fish ovum, a rare delicacy",100)
+#item template is item(id,name,weight,description,value)
+item_caviar=item("caviar","caviar",0.1,"salt-cured fish ovum, a rare delicacy",100,"food")
