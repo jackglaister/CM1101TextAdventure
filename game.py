932e76792508a04caa5af59d6f4b928cbec464da
Peppy-Joe <44087928+Peppy-Joe@users.noreply.github.com>
2018-10-22 13:30:50 +0100

    Add files via upload
    slightly more updated 0.2v

--- game.py
+++ game.py
@@ -1,17 +1,26 @@
-import player, room
+import player, room, os
 def GameControl():
-	player = CreatePlayer()
-	current_room = starterRoom()
+        os.system("cls")
+        player = CreatePlayer()
+        current_room = roomStart()
+        os.system("cls")
+        menu(current_room)
 
 def CreatePlayer():
-	name = input("Please enter your name: ")
-	gender = input("Please enter your gender: ")
-	return player.player(name,gender)
+        name = input("Please enter your name: ")
+        gender = input("Please pick your Gender: Male, Female or Other ")
+        if gender.upper() == ("MALE" or "BOY"):
+                gender = "male"
+        elif gender.upper() == ("FEMALE" or "GIRL"):
+                gender = "female"
+        else:
+                gender = "other"
+        return player.player(name,gender)
 
-def starterRoom():
-	return room.room("start","description",0,0)
+def roomStart():
+        return room.room("the royal kitchen","you are a lowly servant to the Royal Family of Orleasia.",0,0)
 
 def menu(current_room):
-	print(current_room.name+"\n"+current_room.description)
+        print(current_room.name.upper()+"\n\n"+current_room.description)
 
 GameControl()
