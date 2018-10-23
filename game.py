863fa0b15e1f90f12b3ed909d6f07b7fcc6e963c
Jack Glaister <jackglaister@gmail.com>
2018-10-23 20:10:31 +0100

    added eat, drop and pick up functions

--- game.py
+++ game.py
@@ -13,9 +13,9 @@ def GameControl():
 def PrintOptions(current_room, current_player):
     printRoomItems(current_room.items)
     printInventoryItems(current_player.inventory)
+    print("Your health is currently "+str(current_player.health))
     done = False
-    print("\nWhat do you wish to do: ")
-    print("pick up and drop and use?") #make another function to say all this like with the demos we had
+    print("\n What do you wish to do: ")
     printExits(current_room)
     while not done:
         answer = input(">")
@@ -26,18 +26,56 @@ def PrintOptions(current_room, current_player):
             elif current_room.is_valid_exit(answer[1]):
                 current_room = current_room.next(answer[1])
             done = True
-        elif answer[0].upper() == "pick":
+        elif answer[0].upper() == "DROP":
             if len(answer) < 2:
                 answer = input("drop what?")
-                drop(answer, current_player)
+                current_player, current_room = drop(answer, current_player, current_room)
             else:
-                drop(answer[2:len(answer)], current_player, room)
-        
-        #elif's for pick up, drop, consume
+                current_player, current_room = drop(answer[2:len(answer)], current_player, current_room)
+        elif answer[0].upper() == "PICK":
+            if len(answer) < 3:
+                answer = input("pick up what")
+                current_room, current_player = pickup(answer, current_player, current_room)
+            else:
+                current_room, current_player = pickup(answer[2], current_player, current_room)
+        elif (answer[0].upper() == "EAT" or answer[0].upper() == "CONSUME"):
+            if len(answer) < 2:
+                answer = input("Eat what? ")
+                current_player = eat(answer, current_player)
+            else:
+                current_player = eat(answer[1], current_player)
         else:
             print("you what????")
 
-def drop(answer, current_player, room):
+def eat(answer, current_player):
+    newInv = []
+    for item in current_player.inventory:
+        if answer.upper() == item.name.upper():
+            if item.type.upper() == "FOOD":
+                current_player.health += item.potency
+            else:
+                print("I can't eat that!")
+                newInv.append(item)
+        else:
+             newInv.append(item)
+    current_player.inventory = newInv.append(item)
+    return current_player
+
+def pickup(answer, current_player, current_room):
+    roomitems = []
+    for item in current_room.items:
+        if item.name.upper() == answer.upper():
+            current_player.inventory.append(item)
+        else:
+            roomitems.append(item)
+    if len(roomitems) == len(current_room.items):
+        print("you cannot pick that up")
+    else:
+        print("successfully picked up "+answer)
+        current_room.items = roomitems
+        return current_room, current_player
+
+def drop(answer, current_player, current_room):
     found = False
     inventory = []
     for item in current_player.inventory:
@@ -46,6 +84,13 @@ def drop(answer, current_player, room):
             current_room.items.append(item)
         else:
             inventory.append(item.name)
+    if (found):
+        current_player.inventory = inventory
+        return current_player, current_room
+        print("successfully dropped "+answer)
+    else:
+        print("you do not have one of those to drop")
+        return current_player, current_room
 
 def CreatePlayer():
     longish = False
@@ -84,23 +129,30 @@ def menu(current_room, current_player):
 def printRoomItems(itemlist):
     itemslist = listItems(itemlist)
     if itemslist == "":
-        print("there are no carryable items in this room")
+        print("There are no carryable items in this room")
     else:
         print("You can pick up: "+itemslist+" from here.")
 
 def printInventoryItems(inventory):
     itemslist = listItems(inventory)
     if itemslist == "":
-        print("there are no items you can drop")
+        print("There are no items you can drop")
     else:
-        print("you have and can drop "+itemslist)
-
+        print("You have and can drop "+itemslist)
+    edible = []
+    for item in inventory:
+        if item.type == "food":
+            edible.append(item)
+    edibleList = listItems(edible)
+    if len(edibleList) > 0:
+        print("You can also eat: "+edibleList)
+    else:
+        print("You are carrying no edible items")
 def listItems(itemlist):
     itemslist = ""
     for item in itemlist:
         itemslist += item.name+", "
-    return itemslist
-
+    return itemslist[0:len(itemslist)-2]
 
 def printExits(current_room):
     print("You can: ")
