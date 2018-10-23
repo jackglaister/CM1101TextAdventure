275d9b81a36eceac86c6e373ec0c71e286e734e7
jackglaister <44065198+jackglaister@users.noreply.github.com>
2018-10-23 14:10:32 +0100

    updated for room item printing

--- room.py
+++ room.py
@@ -1,6 +1,7 @@
 class room():
-    def __init__(self, name, description, x, exits):	#init class (x and y are location variables)
+    def __init__(self, name, description, x, items, exits):	#init class (x and y are location variables)
         self.name=name
         self.description=description
-        self.number=(x)
-	self.exits=exits
+        self.number=x
+        self.exits=exits
+        self.items=items
