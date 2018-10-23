b69b212b0c1bceb4a35082da058ef778e0b4a5e9
Joe Creek <43994225+CreekKraken@users.noreply.github.com>
2018-10-23 14:13:32 +0100

    formatting and some changes
    might add armour but is not priority. good idea though.

--- items.py
+++ items.py
@@ -10,9 +10,9 @@ class item():
 
 #eg reference "items.caviar" to call upon it in another file (after importing items)
 #food
-itemDict = {
-    "caviar":item("caviar","caviar",0.1,"Salt-cured fish ovum, a rare delicacy",100,"food",30),
-    "oyster":item("oyster","oyster",0.2,"oyster is grown on racks in bags and can be characterized as being salty",100,"food",30),
+items = [
+    "caviar":item("caviar","caviar",0.1,"Salt-cured fish eggs, a rare delicacy",100,"food",30),
+    "oyster":item("oyster","oyster",0.2,"Grown on racks in bags and can be characterized as being salty",100,"food",30),
     "insects":item("insects","insects",0.2,"Small creepy crawlies that can be eaten",20,"food",-10),
     "bread":item("bread","bread",0.3,"A dry, un-appetising loaf, with a crisp outside",5,"food",10),
     "apple":item("apple","apple",0.2,"A shiny, ripe, red fruit",15,"food",15),
@@ -23,17 +23,16 @@ itemDict = {
     "water":item("water","water",0.2,"A bladder full of refreshing cold water",5, "food",5),
     "wine":item("wine","wine",0.3,"A small glass bottle filled with delicious Pinot Noir",50,"food",10),
     "beer":item("beer","beer",0.2,"A hip flask full of cheap beer",5,"food",5),
-    #weapons
-    "kitchenknife":item("knife", "kitchen knife", 3, "A standard 4 inch food prepping sword",50,"weapon",50),
-    "basesword":item("basesword","basesword",15,"A single handed, double edged sword",90,"weapon",75),
+#weapons
+    "kitchenknife":item("knife", "kitchen knife", 3, "A standard 4 inch knife for food prep, could be used as a weapon in an emergency.",50,"weapon",50),
+    "basesword":item("basesword","sword",15,"A single handed, double edged sword",90,"weapon",75),
     "ultimatesword":item("ultimatesword","ultimatesword",20,"A large two handed, heavyweight sword",200,"weapon",150),
-    #armour    I added armour in but idk if there's an armour type 
+#armour    I added armour in but idk if there's an armour type 
     "helmet":item("helmet","helmet",9,"A garnet for protecting the head",60,"armour",30),
     "chestplate":item("chestplate","chestplate",20,"A garnet for protecting the chest",70,"armour",40),
     "leggings":item("leggings","leggings",8,"A garnet for protecting the legs",50,"armour",25),
     "boots":item("boots","boots",5,"A garnet for protecting the feet",40,"armour",25),
-    #potions
+#potions
     "potionhp":item("pot.hp","health potion",0.3,"A potion used to restore 60 hp to the drinker",40,"food",60),
-    #this potion could be used to increase the sword stat for a short period of time 
-    "potiondamage":item("pot.dam","damage potion",0.3,"A potion that causes extra dagame on attacks",40,"weapon",30)
-}
+#this potion could be used to increase the sword stat for a short period of time 
+    "potiondamage":item("pot.dam","damage potion",0.3,"A potion that causes 50% extra damage on attack",40,"weapon",1.5)]
