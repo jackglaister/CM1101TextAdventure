de6d2aeaaddde65ee5799bd698c856bc142ee68c
jackglaister <jackglaister@gmail.com>
2018-10-23 15:40:28 +0100

    updated, print inventory and room items and started a drop function

--- game.py
+++ game.py
@@ -11,18 +11,40 @@ def GameControl():
 
 def PrintOptions(current_room, current_player):
     printRoomItems(current_room.items)
+    printInventoryItems(current_player.inventory)
     done = False
     print("\nWhat do you wish to do: ")
-    print("GO FORETH to travel to the next room")
     print("pick up and drop and use?") #make another function to say all this like with the demos we had
+    printExits(current_room)
     while not done:
         answer = input(">")
-        if answer.upper() == "GO FORETH":
-            #another function to select room and move on 
+        answer = answer.split(" ")
+        if answer[0].upper() == "GO":
+            if answer.length > 3:
+                print("your answer is too long")
+            elif current_room.is_valid_exit(answer[1]):
+                current_room = current_room.next(answer[1])
             done = True
+        elif answer[0].upper() == "pick":
+            if len(answer) < 2:
+                answer = input("drop what?")
+                drop(answer, current_player)
+            else:
+                drop(answer[2:len(answer)], current_player, room)
+        
         #elif's for pick up, drop, consume
         else:
             print("you what????")
+
+def drop(answer, current_player, room):
+    found = False
+    inventory = []
+    for item in current_player.inventory:
+        if answer == item.name:
+            found = True
+            current_room.items.append(item)
+        else:
+            inventory.append(item.name)
 
 def CreatePlayer():
     longish = False
@@ -53,7 +75,7 @@ def CreatePlayer():
     return player.player(name,gender)
 
 def roomStart(current_player):
-    return room.room("the royal kitchen","you are a young servant known by the name "+ current_player.name +".\nYou have fallen ill to a grave illness and have decided to leave\nyour terrible life and seek fame and fortune.",0,[itemDict["bread"],itemDict["fish"],itemDict["honey"],itemDict["chicken"],itemDict["kitchenknife"]],["central","right","left"])
+    return room.room("the royal kitchen","you are a young servant known by the name "+ current_player.name +".\nYou have fallen ill to a grave illness and have decided to leave\nyour terrible life and seek fame and fortune.",0,[itemDict["bread"],itemDict["fish"],itemDict["honey"],itemDict["chicken"],itemDict["kitchenknife"]],["forward","right","left"])
 
 def menu(current_room, current_player):
     print(current_room.name.upper()+"\n\n"+current_room.description)
@@ -65,10 +87,23 @@ def printRoomItems(itemlist):
     else:
         print("You can pick up: "+itemslist+" from here.")
 
+def printInventoryItems(inventory):
+    itemslist = listItems(inventory)
+    if itemslist == "":
+        print("there are no items you can drop")
+    else:
+        print("you have and can drop "+itemslist)
+
 def listItems(itemlist):
     itemslist = ""
     for item in itemlist:
         itemslist += item.name+", "
     return itemslist
-    
+
+
+def printExits(current_room):
+    print("You can: ")
+    for exitr in current_room.exits:
+        print("Go "+exitr)
+
 GameControl()
--- room.py
+++ room.py
@@ -5,3 +5,17 @@ class room():
         self.number=x
         self.exits=exits
         self.items=items
+    def is_valid_exit(self, desired):
+        for exits in self.exits:
+            if exits.upper() == desired.upper():
+                return true
+            else:
+                return false
+    def next(self, answer):
+        x = x + 1
+        if (x % 3) == 0:
+            ##static room at roomstore
+            pass
+        else:
+            ##random room at roomstore
+            pass
