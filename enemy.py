7acc3c3f17aa4ed749a464ea7a1e0f220a489e95
Edrikzz <44277950+Edrikzz@users.noreply.github.com>
2018-10-24 16:13:22 +0100

    updated

--- enemy.py
+++ enemy.py
@@ -42,17 +42,59 @@ class oldlady(enemy):
 class dragonling(enemy):
     def __init__(self):
         self.name = "dragon"
-        self.health = 200
+        self.health = 250
         self.xp = 150
         self.attack = ["wing flap",5]
 
+class soldier(enemy):
+    def __init__(self):
+        self.name = "Soldier"
+        self.health = 100
+        self.xp = 100
+        self.attack = ["Slash",9]
+        #created by edrik
+        
+class mage(enemy):
+    def __init__(self):
+        self.name = "mage"
+        self.health = 90
+        self.xp = 25
+        self.attack = ["Beam",7]
+        #created by edrik
+        
+class swordsman(enemy):
+    def __init__(self):
+        self.name = "Swordsman"
+        self.health = 350
+        self.xp = 200
+        self.attack = ["Slash",15]
+        #created by edrik
+        
+class justicehero(enemy):
+    def __init__(self):
+        self.name = "Hero of Justice"
+        self.health = 600
+        self.xp = 250
+        self.attack = ["Slash of Justice",20]
+        #only appear if player have a negetive karma
+        #created by edrik
+        
+class evilhero(enemy):
+    def __init__(self):
+        self.name = "Prince of the Underworld"
+        self.health = 600
+        self.xp = 250
+        self.attack = ["Evil beam",20]
+        #only appear if player have a bad karma
+        #created by edrik
+        
 class kirill(dragonling):
     def __init__(self):
         self.name = "kirill"
         self.health = 1000
         self.favourite = "caviar"
         self.weakness = "oysters"
-        self.attack = ["fire",10]
+        self.attack = ["fire",35]
     def TakeTurn(self, player):
         player.takeDamage(self.attack[1])
         return player
