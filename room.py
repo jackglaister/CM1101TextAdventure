07cebd97d87b99963ff055dae2deb63a92b4acf9
JamieBlue00 <44201003+JamieBlue00@users.noreply.github.com>
2018-10-24 13:05:52 +0100

    changes to descriptions

--- room.py
+++ room.py
@@ -36,16 +36,16 @@ rooms = [
     "random",
     "random",
     "random", 
-    room("Town Centre/Merchants","Here is a place where you can buy (almost) anything",9,[],[]),
-    "random",
+    room("Town Centre/Merchants","Here is a place where you can buy (almost) anything, use your gold wisely.",9,[],[]),
+    "Kirill Encounter",
     "random",
     "random",
     room("Dragon","You have wondered far into an ominous cave, you heard a roar in the distance and thought you ought to check it out. You have since turned a corner and are faced with a dragon. His name is Kirill and you shall be killed for your trespass",9,[],[])
 ]
 
 RandRooms = [
-    room("Dark Road","An old homeless lady is sat on the edge of a path with a sword she told you was very valuable and was passed down from a long line of ancestors and is offering it for sale for 100 gold",0,[],[]),
-    room("The Highway","An old man is trying to cross a 4 lane wide highway ahead of you",0,0,[]),
-    room("Thief's Lair","A thief has approached you and is demanding everything you have",0,0,[]),
-    room("Bandit attack","A bandit is blocking your path ahead, they haven't yet spotted you but one movement could cost your life",0,0,[])    
+    room("Dark Road","An old homeless lady is sat on the edge of a path with a sword. She tells you it is very valuable and was passed down from a long line of ancestors. She is offering it for sale for 100 gold. Do you take it?",0,[],[]),
+    room("The Highway","An old man is trying to cross a 4 lane wide highway ahead of you. ",0,0,[]),
+    room("Thief's Lair","A thief has approached you and is demanding everything you have. He seems a worthy enemy, you are sure you can take him on.",0,0,[]),
+    room("Bandit attack","A bandit is blocking your path ahead, they haven't yet spotted you but one movement could cost your life. Tread carefully.",0,0,[])    
 ]
